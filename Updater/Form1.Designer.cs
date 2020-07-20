67c06fb69f7aa57d4ec412378740f1c576cf2c4a
Tomattex <55641378+Tomattex@users.noreply.github.com>
2020-07-20 13:13:57 -0300

    Add files via upload

---
+++ Updater/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.updateLabel = new System.Windows.Forms.Label();
+            this.updatesButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.updatesButton);
+            this.groupBox1.Controls.Add(this.updateLabel);
+            this.groupBox1.Controls.Add(this.bunifuGradientPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(781, 126);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Program Updater";
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(55)))));
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Gold;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Yellow;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.White;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.White;
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 5);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(37, 121);
+            this.bunifuGradientPanel1.TabIndex = 1;
+            this.bunifuGradientPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.bunifuGradientPanel1_Paint);
+            // 
+            // updateLabel
+            // 
+            this.updateLabel.AutoSize = true;
+            this.updateLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.updateLabel.Location = new System.Drawing.Point(43, 16);
+            this.updateLabel.Name = "updateLabel";
+            this.updateLabel.Size = new System.Drawing.Size(22, 25);
+            this.updateLabel.TabIndex = 2;
+            this.updateLabel.Text = "a";
+            // 
+            // updatesButton
+            // 
+            this.updatesButton.Location = new System.Drawing.Point(669, 97);
+            this.updatesButton.Name = "updatesButton";
+            this.updatesButton.Size = new System.Drawing.Size(106, 23);
+            this.updatesButton.TabIndex = 3;
+            this.updatesButton.Text = "Button";
+            this.updatesButton.UseVisualStyleBackColor = true;
+            this.updatesButton.Click += new System.EventHandler(this.updatesButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(48, 44);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(237, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(43, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 186);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Program Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.Label updateLabel;
+        private System.Windows.Forms.Button updatesButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Updater/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+
+namespace Updater
+{
+    public partial class Form1 : Form
+    {
+        //variables
+        string version = "1.5"; //replace this to the version of your program
+        string pastebinversion = "https://pastebin.com/raw/gBJ0EAZk"; //replace the pastebin with one that is yours
+        string pastebintitle = "https://pastebin.com/raw/hDW5uA8D"; //replace the pastebin with one that is yours
+        string pastebindescription = "https://pastebin.com/raw/0eLEfK0k"; //replace the pastebin with one that is yours
+        string downloadlink = ""; //put the link with one that is yours.
+
+        //webclient
+        WebClient client = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        //bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Red;
+        //bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Firebrick;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Gold;
+            bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Yellow;
+            updateLabel.Text = "You may need to check for updates";
+            updatesButton.Text = "Check for updates";
+        }
+
+        void CheckForUpdates()
+        {
+            string whatsthelatestversion = client.DownloadString(pastebinversion);
+            string whatsthepastebintitle = client.DownloadString(pastebintitle);
+            string whatsthepastebindescription = client.DownloadString(pastebindescription);
+            if (whatsthelatestversion.Contains(version))
+            {
+                bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.LimeGreen;
+                bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Lime;
+                updateLabel.Text = "This program is up to date";
+                updatesButton.Text = "Check for updates";
+            }
+            else
+            {
+                bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Gold;
+                bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Yellow;
+                updateLabel.Text = "There are some updates pending";
+                updatesButton.Text = "Install updates";
+                MessageBox.Show(whatsthepastebindescription, whatsthepastebintitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void updatesButton_Click(object sender, EventArgs e)
+        {
+            if (updatesButton.Text == "Check for updates")
+            {
+                CheckForUpdates();
+            }
+            else if (updatesButton.Text == "Install updates")
+            {
+                label2.Visible = true;
+                progressBar1.Visible = true;
+                updatesButton.Enabled = false;
+                StartDownload();
+            }
+        }
+
+        private void StartDownload()
+        {
+            Thread thread = new Thread(() => {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileAsync(new Uri(downloadlink), "program.rar");
+            });
+            thread.Start();
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 100;
+                label2.Text = "Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive;
+                progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.LimeGreen;
+                bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Lime;
+                updateLabel.Text = "This program is up to date";
+                label2.Text = "Completed, however, you may need to restart the program or extract the file.";
+            });
+        }
+    }
+}
